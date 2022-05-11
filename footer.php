b60ecd72e057ddf43595c8aaee43001508077ed0
transteph <transteph@protonmail.com>
2022-05-11 12:48:14 -0400

    commit on 'master'

---
+++ footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+			<footer id="site-footer" class="header-footer-group">
+
+				<div class="section-inner">
+
+					<div class="footer-credits">
+
+						<p class="footer-copyright">&copy;
+							<?php
+							echo date_i18n(
+								/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
+								_x( 'Y', 'copyright date format', 'twentytwenty' )
+							);
+							?>
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						</p><!-- .footer-copyright -->
+
+						<?php
+						if ( function_exists( 'the_privacy_policy_link' ) ) {
+							the_privacy_policy_link( '<p class="privacy-policy">', '</p>' );
+						}
+						?>
+
+
+					</div><!-- .footer-credits -->
+
+					
+
+				</div><!-- .section-inner -->
+
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?><!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class(); ?>>
+
+		<?php
+		wp_body_open();
+		?>
+
+		<header id="site-header" class="header-footer-group">
+
+			<div class="header-inner section-inner">
+
+
+				<div class="header-navigation-wrapper">
+
+					<?php
+					if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
+						?>
+
+							<nav class="primary-menu-wrapper" aria-label="<?php echo esc_attr_x( 'Horizontal', 'menu', 'twentytwenty' ); ?>">
+
+								<ul class="primary-menu reset-list-style">
+
+								<?php
+								if ( has_nav_menu( 'primary' ) ) {
+
+									wp_nav_menu(
+										array(
+											'container'  => '',
+											'items_wrap' => '%3$s',
+											'theme_location' => 'primary',
+										)
+									);
+
+								} elseif ( ! has_nav_menu( 'expanded' ) ) {
+
+									wp_list_pages(
+										array(
+											'match_menu_classes' => true,
+											'show_sub_menu_icons' => true,
+											'title_li' => false,
+											'walker'   => new TwentyTwenty_Walker_Page(),
+										)
+									);
+
+								}
+								?>
+
+								</ul>
+
+							</nav><!-- .primary-menu-wrapper -->
+
+						<?php
+					}
+?>
+			
+
+				</div><!-- .header-navigation-wrapper -->
+
+					<div class="header-titles">
+						
+
+						<?php
+							// Site title or logo.
+							twentytwenty_site_logo();
+
+						?>
+<p class="site-description"><span id="emph">W.E.S.U.N.</span><br>Windsor-Essex Supporting Ukrainian Newcomers
+</p>
+					</div><!-- .header-titles -->
+			</div><!-- .header-inner -->
+
+			<?php
+			// Output the search modal (if it is activated in the customizer).
+			if ( true === $enable_header_search ) {
+				get_template_part( 'template-parts/modal-search' );
+			}
+			?>
+
+
+		<?php
+		// Output the menu modal.
+		get_template_part( 'template-parts/modal-menu' );
